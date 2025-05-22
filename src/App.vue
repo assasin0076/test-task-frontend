6e226b5c062b59b1d827c9e817656fac15ea0112
victor <VGeneralov@sila.ru>
2025-05-22 03:52:59 +0200

    feat: loading state

--- src/App.vue
+++ src/App.vue
@@ -118,6 +118,12 @@ onMounted(async () => {
       </div>
     </div>
   </div>
+  <div
+    v-if="isLoading"
+    class="h-screen w-screen fixed top-0 right-0 bg-gray-500/5 backdrop-blur-xs flex items-center justify-center"
+  >
+    <div class="font-bold text-6xl rainbow-text">wait...</div>
+  </div>
 </template>
 
 <style scoped>
@@ -127,15 +133,28 @@ onMounted(async () => {
   animation: gradient-animation 10s infinite;
 }
 
-@keyframes gradient-animation {
+@keyframes rainbow-text-gradient {
   0% {
     background-position: 0% 50%;
   }
-  50% {
+  100% {
     background-position: 100% 50%;
   }
-  100% {
-    background-position: 0% 50%;
-  }
+}
+
+.rainbow-text {
+  background: linear-gradient(
+    270deg,
+    rgba(255, 0, 0, 0.8),
+    rgba(255, 165, 0, 0.8),
+    rgba(0, 0, 255, 0.8),
+    rgba(75, 0, 130, 0.8),
+    rgba(238, 130, 238, 0.8)
+  );
+  background-size: 800% 100%;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: rainbow-text-gradient 8s ease-in-out infinite;
+  text-shadow: 2px 0px 20px rgba(0, 0, 0, 0.2);
 }
 </style>
