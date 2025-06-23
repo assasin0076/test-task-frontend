6c91e18fbb36f41d7fbf5da05ff4a2bd0a0253bb
victor <VGeneralov@sila.ru>
2025-06-23 13:56:57 +0200

    feat: увороты мухи

--- src/components/TheFly.vue
+++ src/components/TheFly.vue
@@ -1,27 +1,49 @@
 <template>
   <teleport to="body">
-    <div ref="fly" class="float-animation transition absolute top-8 left-8 h-2 w-2 bg-gray">
-      <fly-icon class="w-8 h-8 transition" :style="rotationStyle" />
-    </div>
+    <Transition name="fade">
+      <div
+        v-show="isFlyVisible"
+        ref="fly"
+        class="float-animation transition absolute top-8 left-8 h-2 w-2 bg-gray"
+      >
+        <fly-icon
+          v-element-hover="onMouseInElement"
+          class="w-8 h-8 transition"
+          :style="rotationStyle"
+        />
+      </div>
+    </Transition>
   </teleport>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue'
-import { useWindowSize } from '@vueuse/core'
+import { computed, onMounted, ref, shallowRef } from 'vue'
+import { useIntervalFn, useWindowSize } from '@vueuse/core'
 import FlyIcon from '@/components/FlyIcon.vue'
+import { vElementHover } from '@vueuse/components'
 
-const { width, height } = useWindowSize()
+function onMouseInElement() {
+  chaoticChangeDirection()
+  speed.value = 2
+}
+
+const padding = 60
+const baseSpeed = 12
+const speed = shallowRef(baseSpeed)
+
+const isFlyVisible = ref(false)
 
 const fly = ref<HTMLElement | null>(null)
+
+const { width, height } = useWindowSize()
 
 const getRawHtmlValue = (val) => {
   return parseInt(val) || 0
 }
-const isFlyLeftTouch = () => getRawHtmlValue(fly.value.style.left) - 20 >= 40
+const isFlyLeftTouch = () => getRawHtmlValue(fly.value.style.left) - 20 >= padding
 const isFlyRightTouch = () => getRawHtmlValue(fly.value.style.left) + 40 <= width.value
-const isFlyTopTouch = () => getRawHtmlValue(fly.value.style.top) - 20 >= 40
-const isFlyBottomTouch = () => getRawHtmlValue(fly.value.style.top) + 40 <= height.value
+const isFlyTopTouch = () => getRawHtmlValue(fly.value.style.top) - 20 >= padding
+const isFlyBottomTouch = () => getRawHtmlValue(fly.value.style.top) + padding <= height.value
 const moveFlyLeft = () => {
   if (isFlyLeftTouch()) fly.value.style.left = `${getRawHtmlValue(fly.value.style.left) - 1 || 1}px`
   else direction.value = 'right'
@@ -67,9 +89,10 @@ const directionsHandlers = {
 }
 type Direction = 'left' | 'right' | 'up' | 'down' | 'leftUp' | 'leftDown' | 'rightUp' | 'rightDown'
 const direction = ref<Direction>('right')
-setInterval(() => {
+
+const { pause, resume, isActive } = useIntervalFn(() => {
   directionsHandlers[direction.value]()
-}, 12)
+}, speed)
 
 const getRotationStyle = (direction: Direction): { transform: string } => {
   const rotationMap: Record<Direction, number> = {
@@ -101,8 +124,15 @@ const chaoticChangeDirection = () => {
     'rightDown',
   ]
   direction.value = directions[Math.floor(Math.random() * 8)]
+  speed.value = baseSpeed
 }
 setInterval(chaoticChangeDirection, 2000)
+
+onMounted(() => {
+  fly.value.style.left = `${Math.floor(Math.random() * width.value - padding)}px`
+  fly.value.style.top = `${Math.floor(Math.random() * height.value - padding)}px`
+  isFlyVisible.value = true
+})
 </script>
 
 <style>
@@ -126,5 +156,18 @@ setInterval(chaoticChangeDirection, 2000)
   100% {
     transform: translate(0, 0);
   }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition:
+    opacity 2s ease,
+    filter 2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  filter: blur(1.5rem);
 }
 </style>
